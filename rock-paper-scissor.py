ecf68e18b2700aa695663b80dbca87c92fa77c4c
Muhammad-Alinoor <alinoor145387@outlook.com>
2026-03-23 11:40:18 +0600

    choice function created successfully

---
+++ rock-paper-scissor.py
@@ -0,0 +1,10 @@
+import random
+#choice 
+def get_choices():
+    player_choice = input()
+    options = ["rock","paper","scissor"]
+    computer_choice = random.choice(options)
+    choices = {"player":player_choice,"computer":computer_choice}
+    return choices
+response = get_choices()
+print(response)
