b698956429924eb29c2f16538cc25c242b5016a6
Muhammad-Alinoor <alinoor145387@outlook.com>
2026-03-23 11:49:45 +0600

    f string added

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -6,5 +6,11 @@ def get_choices():
     computer_choice = random.choice(options)
     choices = {"player":player_choice,"computer":computer_choice}
     return choices
-response = get_choices()
-print(response)
+#response = get_choices()
+#print(response)
+
+def check_win(player,computer):
+    print(f"You chose {player}, computer chose {computer}")
+    if player == computer:
+        return "It's a Tie!"
+check_win("rock","paper")
